e464cea72d8ad7b9a12d8c0e5a1549edb0a2512c
Joe Rowley <joer14@gmail.com>
2015-03-11 12:20:19 -0700

    added another box for annotations

--- keystone.js
+++ keystone.js
@@ -34,7 +34,7 @@
             // console.log(recs.length);
             //return recs.length;
             //return +recs[0].count;
-            return {party: recs[0].party, count: recs[0].count, valueOf: value}
+            return { count: recs[0].count, valueOf: value}
           });
           function value() { return +this.count; }
           matrix = mpr.getMatrix();
@@ -60,19 +60,87 @@
       //*******************************************************************
       
       function drawChords (matrix, mmap) {
-        var w = 980, h = 800, r1 = h / 2, r0 = r1 - 100;
+        var w = 700, h = 700, r1 = h / 2, r0 = r1 - 100;
 
         var fill = d3.scale.ordinal()
             .range(['#c7b570','#c6cdc7','#335c64','#768935','#507282','#5c4a56','#aa7455','#574109','#837722','#73342d','#0a5564','#9c8f57','#7895a4','#4a5456','#b0a690','#0a3542',]);
-
+console.log(d3.ascending);
         var chord = d3.layout.chord()
             .padding(.01)
             //.sortGroups()
-            .sortSubgroups(d3.ascending)
             //.sortSubgroups(d3.ascending)
-        
-            .sortGroups(d3.descending);
-        //chord.sortSubgroups(d3.descending);
+
+            .sortGroups(
+              /*
+                democrats = 26754682
+                repubs = 58424112
+
+                function d3_ascending(a, b) {
+                return a < b ? -1 : a > b ? 1 : a >= b ? 0 : NaN;
+              }
+
+              */
+              //console.log(d3.ascending);/
+              //d3.ascending
+              function(a,b){ 
+                //console.log("A: ", a, "B: ", b);
+                //if(a==1558488)return 2;
+                //else 
+                
+                //console.log(d3.ascending);
+                //console.log("B: ", b);
+                // A hack so that democrats and republicans
+                // are right next to each other because
+                // as it turns out, fossil fuel energy industry
+                // gives more money than democrats recieve.
+                var fossVal = 26928999;
+                var demoVal = 26754682;
+                var repubVal = 58424112;
+
+                //console.log(a,b);
+                if((a == fossVal)&&(b == demoVal)) return -1;
+                if((b == demoVal)&&(a == repubVal)) return -1;
+                
+                if(a<b) {
+                //  console.log(-1);
+                  return -1;
+                }else if (a>b) {
+                //  console.log(1);
+                  return 1;
+                }else if(a>=0) {
+                //  console.log(0);
+                  return 0;
+                }else {
+                //  console.log(NaN);
+                  return NaN;
+                }
+                //return a < b ? -1 : a > b ? 1 : a >= b ? 0 : NaN;
+                //console.log(d3.ascending);  
+            }
+            )
+            //.sortSubgroups(function(a,b) { return 1; })
+            //.sortChords(d3.ascending)
+            //.sortGroups(d3.descending);
+        chord.sortSubgroups(
+          d3.ascending
+          // function(a,b){ 
+          //       //console.log("A: ", a, "B: ", b);
+          //       //if(a==1558488)return 2;
+          //       //else 
+                
+          //       //console.log(d3.ascending);
+          //       //console.log("B: ", b);
+          //       // A hack so that democrats and republicans
+          //       // are right next to each other because
+          //       // as it turns out, fossil fuel energy industry
+          //       // gives more money than democrats recieve.
+                
+          //       console.log(a,b);
+          //       //return d3.ascneding
+          //       //return a < b ? -1 : a > b ? 1 : a >= b ? 0 : NaN;
+          //       //console.log(d3.ascending);  
+          //   }
+            );
         
         // console.log(chord.groups)
         var arc = d3.svg.arc()
@@ -82,10 +150,16 @@
         var svg = d3.select("#chartBox").append("svg:svg")
             .attr("width", w)
             .attr("height", h)
+            .attr("id","chordDiagram")
           .append("svg:g")
             .attr("id", "circle")
-            .attr("transform", "translate(" + w / 2 + "," + h / 2 + ")");
-
+            .attr("transform", function(){
+              var trans= "translate(" + w / 2 + "," + h / 2 + ")";
+              var trans2 = "scale(1,-1)";
+              var trans3= trans + trans2;
+              return trans3;
+            })
+            
             svg.append("circle")
                 .attr("r", r0 + 20);
 
@@ -98,7 +172,7 @@
           .enter().append("svg:g")
             .attr("class", "group")
             .on("mouseover", mouseover)
-            .on("mouseout", function (d) { d3.select("#tooltip").style("visibility", "hidden") });
+            .on("mouseout", mouseout);
 
         g.append("svg:path")
             .style("stroke", "black")
@@ -115,17 +189,61 @@
             .attr("d", arc);
 
         g.append("svg:text")
-            .each(function(d) { d.angle = (d.startAngle + d.endAngle) / 2; })
-            .attr("dy", ".35em")
-            .style("font-family", "helvetica, arial, sans-serif")
-            .style("font-size", "10px")
-            .attr("text-anchor", function(d) { return d.angle > Math.PI ? "end" : null; })
-            .attr("transform", function(d) {
-              return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")"
-                  + "translate(" + (r0 + 26) + ")"
-                  + (d.angle > Math.PI ? "rotate(180)" : "");
-            })
-            .text(function(d) { return rdr(d).gname; });
+            .each(function(d) { 
+              d.angle = (d.startAngle + d.endAngle) / 2; 
+              //d.party = "joe";
+              })
+              .attr("dy", ".35em")
+              .style("font-family", "helvetica, arial, sans-serif")
+              .style("font-size", "12px")
+              .attr("id", function(d){
+                return "text"+d.index;
+              })
+              .attr("text-anchor", function(d) { 
+                return d.angle > Math.PI ? "end" : null;
+              })
+              // Space out all the text
+              .attr("transform", function(d) {
+                  var trans =  "rotate(" + (d.angle * 180 / Math.PI - 90) + ")"
+                     + "translate(" + (r0 + 26) + ")"  // pushes it out to a certain radius
+                     + (d.angle > Math.PI ? "rotate(180)" : "");
+                  
+                  var yUp = 280;
+                  
+                  //if(d.angle<.12) yUp = 280 + (d.value/89000 )*3;
+                  /*
+                  89000
+                  435900
+                  595292
+                  1518891
+                  */
+                  if(d.value<89052) yUp += 0;
+                  else if(d.value<435905) yUp += 10;
+                  else if(d.value<595293) yUp += 20;
+                  else if(d.value<1518892) yUp += 30;
+                  
+                  var xTrans = Math.sin(d.angle)*280;
+                  var yTrans = -Math.cos(d.angle)*yUp;
+                  //console
+
+                  var trans2 = "translate("+xTrans+","+ yTrans+")"
+                  
+                  var trans3 = " scale(1, -1)";
+
+                  var allTrans = trans2 + trans3;
+                  return allTrans;
+                    //  return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")"
+                    // + "translate(" + (r0 + 26) + ")"
+                    // + (d.angle > Math.PI ? "rotate(180)" : "");
+              })
+              .style("visibility",function(d){
+                if(d.value<1518892) return "hidden";
+                else return ""
+              })
+              
+              .text(function(d) { 
+                return rdr(d).gname; 
+              });
 
           
        // chord.sortSubGroups(d3.descending)
@@ -168,21 +286,23 @@
                     .style("top", function () { return (d3.event.pageY - 170)+"px"})
                     .style("left", function () { return (d3.event.pageX - 100)+"px";})
                 })
+               
                 .on("mouseout", function (d) { d3.select("#tooltip").style("visibility", "hidden") });  
-          function chordTip (d) {
-            var p = d3.format(".1%"), q = d3.format(",f")
-             //console.log(d);
-             //console.log(d.tname);
-            var msg= "<strong>Contribution Info:</strong><br/>"
-              
+                  function chordTip (d) {
+                    var p = d3.format(".1%"), q = d3.format(",f")
+                     //console.log(d);
+                     //console.log(d.tname);
+                    var msg= "<strong>Contribution Info:</strong><br/>"
+                      
 
-              
-              + d.tname + " to " + d.sname +"s"
-              + ": $" + q(d.tvalue) + "<br/>"
-              + p(d.tvalue/d.ttotal) + " of " + d.tname + "'s Total ($" + q(d.ttotal) + ")<br/>"
-              + p(d.tvalue/(d.mtotal/2)) + " of Total Donated($" + q(d.mtotal/2) + ")";
-            return msg;
-          }
+                      
+                      + d.tname + " to " + d.sname +"s"
+                      + ": $" + q(d.tvalue) + "<br/>"
+                      + p(d.tvalue/d.ttotal) + " of " + d.tname + "'s Total ($" + q(d.ttotal) + ")<br/>"
+                      + p(d.tvalue/(d.mtotal/2)) + " of Total Donated($" + q(d.mtotal/2) + ")";
+                    return msg;
+                  }
+
 
           function groupTip (d) {
             // console.log( d);
@@ -197,6 +317,42 @@
           
           }
 
+          function fillInfoBox (d) {
+            // console.log( d);
+            var p = d3.format(".1%"), q = d3.format(",f")
+            
+            // Fill in all the sub groups and how much they donated here
+            // Maybe fill in special text for each one too.. idk. 
+            var subGroups = { 
+                            
+                            "Fossil Fuels": {"ILBC":"$10","SUA":"$1000"},
+                            "Construction": {"ILBC":"$10","SUA":"$1000"},
+                            "Unions": {"ILBC":"$10","SUA":"$1000"},
+                            "Ideological Groups": {"dumb shit":"$10","SUA":"$1000"},
+                            "Transportation": {"cars":"$10","SUA":"$1000"},
+                            "Environmental policy": {"ILBC":"$10","SUA":"$1000"},
+                            "Business Associations": {"ILBC":"$10","SUA":"$1000"},
+                            "Alternate energy production & services": {"ILBC":"$10","SUA":"$1000"},
+                            "Nuclear energy": {"ILBC":"$10","SUA":"$1000"},
+                            "Other": {"ILBC":"$10","SUA":"$1000"},
+                            "Materials & Manufacturing": {"ILBC":"$10","SUA":"$1000"}
+
+                            };
+            //_each
+            var strGroupsList = ""; 
+            
+            var aGroup= subGroups[d.gname];
+            _.forEach(aGroup, function(n, key) {
+              strGroupsList += "<li>"+ n +": " + key + "</li>";
+            });
+            var header = "<strong>" +d.gname + ":</strong> <br>";
+            var strGroups = "Includes: <ul>" + strGroupsList +"</ul>";
+            var strTotalDonate = "Contributed: $" + q(d.gvalue) + " (" + p(d.gvalue/(d.mtotal/2)) +" of total donations) <br>";
+            var msg = header + strTotalDonate + strGroups ;
+            return msg;
+          
+          }
+
           function mouseover(d, i) {
             d3.select("#tooltip")
               .style("visibility", "visible")
@@ -204,9 +360,34 @@
               .style("top", function () { return (d3.event.pageY - 80)+"px"})
               .style("left", function () { return (d3.event.pageX - 130)+"px";})
 
+            
+             
+            d3.select("#infoBox")
+              .style("visibility", "visible")
+              .html(groupTip(rdr(d)))
+
+            d3.select("#infoBox")
+              .style("visibility", "visible")
+              .html(fillInfoBox(rdr(d)))
+
+
+            var textStringID = "#text"+i;      
+            d3.select(textStringID)
+              .style("visibility", "visible")
+
             chordPaths.classed("fade", function(p) {
               return p.source.index != i
                   && p.target.index != i;
             });
+          }
+          function mouseout(d, i) {
+            
+            d3.select("#tooltip").style("visibility", "hidden") 
+            //Only remove text labels for small tight groups
+            if(d.value<1518892) {
+              var textStringID = "#text"+i;      
+              d3.select(textStringID)
+                .style("visibility", "hidden")
+            }
           }
       }
